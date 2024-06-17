bbd6df258a88b4561e21ba0af9c39d66563e3500
Azhar Khan <107482530+Alkhanazhar@users.noreply.github.com>
2024-06-18 00:53:04 +0530

    blog

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+
+function App() {
+  console.log(import.meta.env.VITE_API_Endpoint)
+  return <>app</>;
+}
+
+export default App;
---
+++ client/src/conf/conf.js
@@ -0,0 +1,8 @@
+const conf = {
+    VITE_API_Endpoint: String("https://cloud.appwrite.io/v1"),
+    VITE_Project_ID: String("666e9ef4000054b9f0dc"),
+    VITE_Database_id: String("666eb45300318b1f3539"),
+    VITE_Collection_id: String("666eb45300318b1f3539"),
+    VITE_Bucket_id: String("666f23960035432f4d12"),
+}
+export default conf;
